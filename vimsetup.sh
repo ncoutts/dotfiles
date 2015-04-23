97eebe0e2e59cf58a9b947e6b3a9dc3a21f5e7b7
Nick Coutts <ncoutts@users.noreply.github.com>
2015-04-23 18:18:29 -0400

    Tweaks vimsetup script after running on fresh system

--- vimsetup.sh
+++ vimsetup.sh
@@ -2,13 +2,12 @@
 mkdir ~/.vim
 mkdir ~/.vim/autoload
 mkdir ~/.vim/bundle
-mdkir ~/.vim/colors
-git clone https://github.com/bling/vim-airline.git ~/.vim/bundle
-git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle
+mkdir ~/.vim/colors
+mkdir ~/.vim/tmp
+mkdir ~/.vim/tmp/backup
+mkdir ~/.vim/tmp/swap
+mkdir ~/.vim/tmp/undo
+git clone https://github.com/bling/vim-airline.git ~/.vim/bundle/vim-airline
+git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 cd ~/.vim/colors && wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim && cd --
 cd ~/.vim/autoload && wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim && cd --
-
-mkdir ~/tmp
-mkdir ~/tmp/backup
-mkdir ~/tmp/swap
-mkdir ~/tmp/undo
