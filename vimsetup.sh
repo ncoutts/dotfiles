c4b90688ae331aa635334a16110b69592a239074
Nick Coutts <ncoutts@users.noreply.github.com>
2015-04-27 13:18:38 -0400

    Adds typescript syntax plugin to vim setup

--- vimsetup.sh
+++ vimsetup.sh
@@ -9,6 +9,7 @@ mkdir ~/.vim/tmp/swap
 mkdir ~/.vim/tmp/undo
 git clone https://github.com/bling/vim-airline.git ~/.vim/bundle/vim-airline
 git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
-git clone https://github.com/wookiehangover/jshint.vim.git ~/.vim/bundle/jshint.vim
+git clone https://github.com/wookiehangover/jshint.vim.git ~/.vim/bundle/jshint-vim
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/bundle/typescript-vim
 cd ~/.vim/colors && wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim && cd --
 cd ~/.vim/autoload && wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim && cd --
